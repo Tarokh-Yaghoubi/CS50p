d33db3854d841e8443dc2aea5ebb256c793448f9
Tarokh Yaghoubi <tarokhgit@gmail.com>
2023-03-18 16:04:55 +0330

    a practice on Dictionaries in Python programming

---
+++ hogwarts.py
@@ -0,0 +1,12 @@
+students = {
+    
+    "Hermione" : "Gryffindor",
+    "Harry" : "Gryffindor",
+    "Ron" : "Gryffindor",
+    "Tarokh" : "Koln"
+}
+# we could use _ , but we dont because our code will be to cryptic 
+
+for i in students:
+    print(i, students[i], sep=" ->  ")
+    
