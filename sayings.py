1afdf99c7c7385403254078f2efead738f1e5805
Tarokh Yaghoubi <tarokhgit@gmail.com>
2023-03-24 13:22:04 +0430

    wrote 2 functions to be used in other programs

---
+++ sayings.py
@@ -0,0 +1,15 @@
+def main():
+    hello("World")
+    goodbye("World")
+
+def hello(name):
+    print(f"hello, {name}")
+    
+def goodbye(name):
+    print(f"Good Bye {name}")
+    
+
+
+if __name__ == "__main__":
+
+    main()
