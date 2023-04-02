6c04af5884e33c478469af560b32edc579ff74f2
Tarokh Yaghoubi <tarokhgit@gmail.com>
2023-04-03 00:25:00 +0430

    just a test file for line.py

---
+++ lines/testing.py
@@ -0,0 +1,20 @@
+# this is a comment that must not count as a line 
+
+print("tarokh is here")
+print("tarokh is everywhere")
+
+
+
+
+# tarokh is atarokh 
+
+
+
+
+
+
+
+
+
+
+print("this is a black board")
