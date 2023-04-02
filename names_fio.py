3a67960dd7aebf084342e50a2c15ea4bf326c0ac
Tarokh Yaghoubi <tarokhgit@gmail.com>
2023-04-02 09:36:21 +0430

    how to mine data from csv files and add them to a dict

--- names_fio.py
+++ names_fio.py
@@ -1,14 +1,13 @@
 students = []
 
 with open("names.csv", "r") as file:
-
     for line in file:
-        student = {}
         name, city = line.rstrip().split(",")
-        student["name"] = name
-        student["city"] = city
+        student = {"name": name, "city": city}
         students.append(student)
-        
+    
+def get_name(student):
+    return student['name']
 
-for student in students:
-    print(f"{student['name']} is living in {student['city']}")
+for student in sorted(students, key= lambda student: student['city'], reverse=True):
+    print(f"{student['name']} lives in {student['city']}")
